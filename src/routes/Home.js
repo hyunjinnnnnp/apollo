665cf92c629f45da9091de1e4725d5e5947b142b
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-08-24 14:20:24 +0900

    apollo client

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,23 @@
-const Home = () => "Home";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  if (data && data.movies)
+    return data.movies.map((movie) => <h1>{movie.id}</h1>);
+  return <h1>Home</h1>;
+};
+
 export default Home;
